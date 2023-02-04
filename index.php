aa759dbd10dbc40dcaa35aa477e76a2685c04520
CodeSevenic <sevenicbookings@gmail.com>
2023-02-04 19:17:19 +0200

    Nettel code update

--- index.php
+++ index.php
@@ -7,9 +7,40 @@
 get_header();
 ?>
 
-<div class="content">
-  Content Blog
-</div>
+    <div class="nettel-content">
+        <main class="nettel-main" role="main">
+            <?php
+            if (have_posts()) { ?>
+                <div class="nettel-container">
+                    <!--         Blog Title           -->
+                    <?php
+                    if (is_home() && !is_front_page()) { ?>
+                        <header>
+                            <h1 class="page-title">
+                                <?php single_post_title(); ?>
+                            </h1>
+                        </header>
+                    <?php }
+
+                    ?>
+
+                    <div class="nettel-blog-index">
+
+                        <?php
+                        while (have_posts()) : the_post(); ?>
+                            <div class="blog-index-post">
+                                <p class="post-tag">Post Tag</p>
+                                <h4 class="post-title"><?php the_title(); ?></h4>
+                                <div class="post-excerpt"><?php the_excerpt(); ?></div>
+                            </div>
+                        <?php endwhile;
+                        ?>
+                    </div>
+                </div>
+            <?php }
+            ?>
+        </main>
+    </div>
 
 <?php
 get_footer();
