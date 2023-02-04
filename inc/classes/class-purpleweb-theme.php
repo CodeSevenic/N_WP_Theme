1db77980d6e97ef6874bbb969588c2d5bf6dbfa5
CodeSevenic <sevenicbookings@gmail.com>
2023-02-04 07:49:41 +0200

    Nettel code update

--- inc/classes/class-purpleweb-theme.php
+++ inc/classes/class-purpleweb-theme.php
@@ -45,6 +45,30 @@ class PURPLEWEB_THEME {
         ] );
 
         add_theme_support('post-thumbnails');
+
+        add_theme_support('customize-selective-refresh-widgets');
+
+        add_theme_support('automatic-feed-links');
+
+        add_theme_support(
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style'
+            ]
+        );
+
+        add_editor_style();
+        add_theme_support('wp-block-styles');
+
+        add_theme_support('align-wide');
+
+
     }
 
 
