2942720c4e23f6743a7cced51b376c042fa2a742
CodeSevenic <sevenicbookings@gmail.com>
2023-02-05 06:40:31 +0200

    Nettel code update

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -1,1 +1,31 @@
 <?php
+/**
+ * Register Meta Boxes
+ *
+ * @package PurpleWeb
+ */
+
+namespace PURPLEWEB_THEME\Inc;
+
+
+use PURPLEWEB_THEME\Inc\Traits\Singleton;
+
+class Meta_Boxes {
+    use Singleton;
+
+
+    protected function __construct() {
+        // load class.
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks() {
+        /**
+         * Actions.
+         */
+        add_action('wp_enqueue_scripts', [$this, 'register_styles']);
+        add_action('wp_enqueue_scripts', [$this, 'register_scripts']);
+
+    }
+
+}
