db4024d987336fac34031efba889ad1ee2ecd532
CodeSevenic <sevenicbookings@gmail.com>
2023-02-05 05:54:03 +0200

    Nettel code update

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -1,1 +1,21 @@
 <?php
+/**
+ * Template for post-entry header
+ *
+ * @package purbleWeb
+ */
+
+$the_post_id = get_the_ID();
+$has_post_thumbnail = get_the_post_thumbnail($the_post_id);
+?>
+
+<header class="entry-header">
+    <?php
+    // Feature image
+    if ($has_post_thumbnail) { ?>
+        <div class="entry-image">
+            <a href="<?php esc_url(get_permalink()); ?>"></a>
+        </div>
+    <?php }
+    ?>
+</header>
--- template-parts/content.php
+++ template-parts/content.php
@@ -6,8 +6,8 @@
  */
 ?>
 
-<div class="blog-index-post">
+<article id="post-<?php the_ID(); ?>" <?php post_class('blog-index-post'); ?>>
     <p class="post-tag">Post Tag</p>
     <h4 class="post-title"><?php the_title(); ?></h4>
     <div class="post-excerpt"><?php the_excerpt(); ?></div>
-</div>
+</article>
