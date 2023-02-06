4e7edc596847aed81977af9644b1f0e7c745ad98
CodeSevenic <sevenicbookings@gmail.com>
2023-02-06 18:15:24 +0200

    Nettel code update

--- searchform.php
+++ searchform.php
@@ -5,9 +5,8 @@
  * @package purpleWeb
  */
 ?>
-
 <form role="search" method="get" class="nettel-search-form" action="<?php esc_url(home_url('/')); ?>">
-    <span class="screen-reader-text">'. .'</span>
+    <span class="screen-reader-text"><?php echo _x('Search for:', 'label', 'nettel'); ?></span>
     <input type="search" class="form-control" placeholder="<?php echo esc_attr_x('Search','placeholder', 'nettel') ?>" value="<?php the_search_query(); ?>" name="s" aria-label="Search">
-    <button class="nettel-search-btn" type="submit">Search</button>
+    <button class="nettel-search-btn" type="submit">RCM</button>
 </form>
