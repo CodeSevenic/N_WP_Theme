9d69aad8014470c27359b35cdd263238f6058baa
CodeSevenic <sevenicbookings@gmail.com>
2023-02-10 12:08:23 +0200

    Nettel Code Update

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 /**
  * Header template.
- * 
+ *
  * @package purpleWeb
  */
 ?>
@@ -10,18 +10,17 @@
 <html lang="<?php language_attributes(); ?>">
 
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head() ?>
-
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head() ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php
-    if (function_exists('wp_body_open')) {
-        wp_body_open();
-    }
+if (function_exists('wp_body_open')) {
+    wp_body_open();
+}
 ?>
 <div id="page" class="site">
     <header id="masthead" class="site-header" role="banner">
