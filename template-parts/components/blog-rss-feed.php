8dc937548b6db7c91fdcc25bd7ba2b44f0c4a340
CodeSevenic <sevenicbookings@gmail.com>
2023-02-16 05:21:01 +0200

    Nettel code update

--- template-parts/components/blog-rss-feed.php
+++ template-parts/components/blog-rss-feed.php
@@ -7,7 +7,7 @@ $args = array(
 
 // Check if tag is set in the URL
 if (isset($_GET['tag'])) {
-    $args['tag'] = $_GET['tag'];
+    // $args['tag'] = $_GET['tag'];
 }
 
 $posts = get_posts($args); ?>
@@ -19,10 +19,11 @@ $posts = get_posts($args); ?>
                 foreach ($posts as $post) {
                     setup_postdata($post);
                     // Display the post title and content
-                    ?>
+            ?>
                     <article id="post-<?php the_ID(); ?>" <?php post_class('blog-index-post'); ?>>
                         <?php get_template_part('template-parts/components/blog/entry-header'); ?>
-                        <?php // get_template_part('template-parts/components/blog/entry-meta'); ?>
+                        <?php // get_template_part('template-parts/components/blog/entry-meta'); 
+                        ?>
                         <?php get_template_part('template-parts/components/blog/entry-tax'); ?>
                         <h4 class="post-title"><a href="<?php echo esc_url(get_permalink()); ?>">
                                 <?php purpleweb_the_title(50); ?></a>
@@ -30,7 +31,8 @@ $posts = get_posts($args); ?>
                         <div class="post-excerpt">
                             <p><?php purpleweb_the_excerpt(120); ?></p>
                         </div>
-                        <?php //echo purpleweb_excerpt_more() ?>
+                        <?php //echo purpleweb_excerpt_more() 
+                        ?>
                         <?php
                         echo do_shortcode('[rt_reading_time postfix="min" postfix_singular="min"]');
                         ?>
@@ -38,7 +40,7 @@ $posts = get_posts($args); ?>
 
                     <?php ?>
 
-                <?php }
+            <?php }
                 wp_reset_postdata();
             } else {
                 echo 'No posts found';
@@ -50,4 +52,3 @@ $posts = get_posts($args); ?>
         </div>
     </div>
 </section>
-
