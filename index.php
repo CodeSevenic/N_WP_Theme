8b4aa0770f18f6d3a2f0b4cec2a992d979d4a358
CodeSevenic <sevenicbookings@gmail.com>
2023-02-20 20:25:25 +0200

    Nettel code update

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ get_header();
                     <div class="nettel-blog-index">
                         <?php
                         while (have_posts()) : the_post(); ?>
+                        
                             <?php get_template_part('template-parts/content-blog'); ?>
                         <?php endwhile;
                         ?>
