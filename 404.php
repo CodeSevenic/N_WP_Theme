b99f7cd1a2704a2af01412b7e9612483c23fd9ce
CodeSevenic <sevenicbookings@gmail.com>
2023-02-24 09:31:14 +0200

    Nettel code update

--- 404.php
+++ 404.php
@@ -5,9 +5,10 @@
  * @package purpleWeb
  */
 
-get_header()
+get_header();
+$page404img = wp_get_attachment_image_src(108,'large', true);
 ?>
 
-    <div class="nettel-404"></div>
+    <div class="nettel-404" style="background-image: url('<?php echo $page404img[0]  ?>')"></div>
 
 <?php get_footer() ?>
