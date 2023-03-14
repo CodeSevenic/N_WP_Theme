7c0123a4412c3744e8b0270c67c73f11265922ab
CodeSevenic <sevenicbookings@gmail.com>
2023-03-14 12:39:19 +0200

    Nettel code update

--- header.php
+++ header.php
@@ -13,6 +13,14 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TMDPNMRWXE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-TMDPNMRWXE');
+    </script>
     <?php wp_head() ?>
 </head>
 
